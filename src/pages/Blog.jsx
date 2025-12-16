198dfcbf7ad7337e4b9b7113446c8a719ca67338
yasinarafat5353 <yasinrafat.cid.bd@gmail.com>
2025-12-16 21:41:59 +0600

    add Comment Section

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,22 +1,30 @@
 import  { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {assets, blog_data} from '../assets/assets'
+import {assets, blog_data, comments_data} from '../assets/assets'
 import Navber from '../components/Navber'
 import moment from 'moment'
+import { div } from 'motion/react-client'
 
 export default function Blog() {
 
   const {id} = useParams()
 
   const [data, setData] = useState(null)
+  const [comments, setComments] = useState([])
 
   const fetchBlogData = async ()=>{
   const data = blog_data.find(item => item._id ===id)
   setData(data)
   }
 
+  const fetchComments = async () => {
+    setComments(comments_data)
+  }
+
+
   useEffect(() => {
     fetchBlogData()
+    fetchComments()
   }, [])
 
   return data ? (
@@ -35,6 +43,24 @@ export default function Blog() {
       <img src={data.image} alt="" className='rounded-3xl mb-5'/>
 
       <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}></div>
+
+        {/* Comment Section  */}
+      <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+        <p>Comments {comments.length}</p>
+        <div className='flex flex-col gap-4'>
+          {comments.map((item, index)=>(
+            <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
+              <div className='flex items-center gap-2 mb-2'>
+                <img src={assets.user_icon} alt="" className='w-6' />
+                <p className='font-medium'>{item.name}</p>
+              </div>
+              <p className='text-sm max-w-md ml-8 '>{item.content}</p>
+              <div className='absolute right-4 bottom-3 flex items-center gap-2 texxt-sx'>{moment(item.createdAt).fromNow()}</div>
+            </div>
+          ))}
+
+        </div>
+      </div>
 
     </div>
 
