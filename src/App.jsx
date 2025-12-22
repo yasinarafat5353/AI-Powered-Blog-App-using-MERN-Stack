3263713a800c3095d683734821f39d7ebb6a3e32
yasinarafat5353 <yasinrafat.cid.bd@gmail.com>
2025-12-22 08:51:58 +0700

    add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import Dashbord from "./pages/admin/Dashbord";
 import AddBlog from "./pages/admin/AddBlog";
 import ListBlog from "./pages/admin/ListBlog";
 import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 export default function App() {
+  const isAuthenticated = false; // Replace with actual auth logic
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/blog/:id" element={<Blog/>} />
-        <Route path="/admin" element={<Layout/>}>
+        <Route path="/admin" element={isAuthenticated ? <Layout/> : <Login/>}>
           <Route index element={<Dashbord/>} />
           <Route path="addBlog" element={<AddBlog/>} />
           <Route path="listBlog" element={<ListBlog/>} />
--- src/pages/admin/Layout.jsx
+++ src/pages/admin/Layout.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 
+import {assets} from '../../assets/assets'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/admin/Sidebar'
 const Layout = () => {
+
+    const navigaten = useNavigate()
+
+    const logout = ()=> {
+      navigaten('/')
+    }
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex items-center justify-between py-2 h-17.5 px-4 sm:px-12 borde-b border-gray-200'>
+        <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=>navigaten('/')}/>
+        <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer '>Logout</button>
+
+      </div>
+      <div className='flex h-[calc(100vh-70px)] '>
+        <Sidebar/>
+        <Outlet/>
+      </div>
+    </>
   )
 }
 
